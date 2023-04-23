5930d8b6b9858b5b34a62132548e2ff282fbd8ab
jonwestman <jonwestman88@gmail.com>
2023-04-23 21:32:17 +0200

    added primary keys

--- Labb2LINQ/Models/Course.cs
+++ Labb2LINQ/Models/Course.cs
@@ -1,6 +1,12 @@
-﻿namespace Labb2LINQ.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Labb2LINQ.Models
 {
     public class Course
     {
+        [Key]
+        [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+        public int CourseId { get; set; }
     }
 }
--- Labb2LINQ/Models/Teacher.cs
+++ Labb2LINQ/Models/Teacher.cs
@@ -1,6 +1,14 @@
-﻿namespace Labb2LINQ.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Labb2LINQ.Models
 {
     public class Teacher
     {
+        [Key]
+        [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
