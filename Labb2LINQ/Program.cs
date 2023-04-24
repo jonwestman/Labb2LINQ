bbb8ab9aaca486c2dfbff9cf46d184a7bcd6a1e0
jonwestman <jonwestman88@gmail.com>
2023-04-24 22:44:09 +0200

    updated connection string and added a service to program.cs

--- Labb2LINQ/Program.cs
+++ Labb2LINQ/Program.cs
@@ -1,3 +1,7 @@
+using Labb2LINQ.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Labb2LINQ
 {
     public class Program
@@ -8,6 +12,9 @@ namespace Labb2LINQ
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<SchoolDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
